7d1f7481a37b1d29ef6884fb750e2356feb3c191
nyananu <35955246+nyananu@users.noreply.github.com>
2020-07-20 10:42:03 -0700

    Add files via upload

---
+++ lab 1/src/test/ca/ubc/cpsc210/nowserving/test/CustomerCounterTest.java
@@ -0,0 +1,40 @@
+package ca.ubc.cpsc210.nowserving.test;
+
+import ca.ubc.cpsc210.nowserving.model.CustomerCounter;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+// Unit tests for the CustomerCounter class
+class CustomerCounterTest {
+    private CustomerCounter testCounter;
+    
+    @BeforeEach
+    void runBefore() {
+        testCounter = new CustomerCounter();
+    }
+    
+    @Test
+    void testConstructor() {
+        assertEquals(1, testCounter.getNextCustomer());
+    }
+    
+    @Test
+    void testNextCustomer() {
+        testCounter.nextCustomer();
+        assertEquals(2, testCounter.getNextCustomer());
+        
+        testCounter.nextCustomer();
+        assertEquals(3, testCounter.getNextCustomer());
+    }
+
+    @Test
+    void testNextCustomerRollAround() {
+        for(int i = 0; i < CustomerCounter.MAX_CUSTOMER_NUMBER; i++) {
+            testCounter.nextCustomer();
+        }
+
+        assertEquals(1, testCounter.getNextCustomer());
+    }
+}
